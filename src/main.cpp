78621deba51ba022ac259f9c4a6d417cb8054ed1
shema102 <sergeishemshur@gmail.com>
2024-06-20 11:01:48 +0300

    feat: implemented basic led test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
 #include "rtcTime.h"
+
+const int NUM_LEDS = 4 * 7 * 2 + 2; // 4 numbers of 7 segments, each segment has 2 leds, plus 2 dots
+CRGB leds[NUM_LEDS];
+
+void drawAllRed();
+void drawAllBlue();
 
 void setup() {
     Serial.begin(9600);
@@ -19,6 +26,8 @@ void setup() {
     } else {
         Serial.println("Time set");
     }
+
+    FastLED.addLeds<NEOPIXEL, 14>(leds, NUM_LEDS);
 }
 
 void loop() {
@@ -37,6 +46,25 @@ void loop() {
     Serial.print(now.second(), DEC);
     Serial.println();
 
-    delay(1000);
+    drawAllRed();
+
+    delay(20);
+
+    drawAllBlue();
+
+    delay(20);
 }
 
+void drawAllRed() {
+    for (auto & led : leds) {
+        led = CRGB::Red;
+    }
+    FastLED.show();
+}
+
+void drawAllBlue() {
+    for (auto & led : leds) {
+        led = CRGB::Blue;
+    }
+    FastLED.show();
+}
